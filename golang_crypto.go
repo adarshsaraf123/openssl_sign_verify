ef18cd6c0d58bb7edfe60f924772af8a074449b9
Adarsh Saraf <adasaraf@in.ibm.com>
2018-06-13 17:54:46 +0530

    add signing capabilities in golang

--- golang_crypto.go
+++ golang_crypto.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"crypto"
 	"crypto/x509"
+	_ "crypto/sha256"
+	// _ "crypto/ecdsa"
+	"crypto/rand"
 	"encoding/pem"
 	"fmt"
 	"io/ioutil"
@@ -25,4 +29,18 @@ func main() {
 	privateKey, err := x509.ParseECPrivateKey(pemBlock.Bytes)
 	panic_on_error(err)
 	fmt.Print(privateKey)
+
+	// obtain the digest
+	message, err := ioutil.ReadFile("data.txt")
+	hashfunc := crypto.SHA256
+	h := hashfunc.New()
+	h.Write(message)
+	digest := h.Sum(nil)
+
+	// sign the digest
+	signature, err := privateKey.Sign(rand.Reader, digest, hashfunc)
+
+	fmt.Print(signature)
+	err = ioutil.WriteFile("signature.bin", signature, 0666)
+	panic_on_error(err)
 }
